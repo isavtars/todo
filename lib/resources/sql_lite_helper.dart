cca41744e3925661b781dcc39d57df5c16427466
isavtars <113200845+isavtars@users.noreply.github.com>
2023-07-09 12:36:48 +0545

    crud ok

--- lib/resources/sql_lite_helper.dart
+++ lib/resources/sql_lite_helper.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+
 import 'package:logger/logger.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqflite.dart';
