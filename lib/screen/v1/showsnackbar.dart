b5f18ff6c88faf7246513bfccc5741f87d15782e
Santosh <santosh.poudel676@gmail.com>
2023-07-25 16:17:30 +0545

    worked on task delete button
    added user confirmation dialog box before deleting the task and shown a snackbar if the task is deleted successfully

--- lib/screen/v1/showsnackbar.dart
+++ lib/screen/v1/showsnackbar.dart
@@ -1,5 +1,35 @@
+// import 'package:flutter/material.dart';
+
+// showSnackbar(BuildContext context, String text) {
+//   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+// }
+
 import 'package:flutter/material.dart';
 
-showSnackbar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(text)));
+class CustomSnackBar extends StatelessWidget {
+  final String text;
+
+  const CustomSnackBar({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.green, // Set the background color
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white, // Set the text color
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
 }
+
